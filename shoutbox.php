712fed13fed657ac0664ed404e25635f4b83d6f1
AdamBeijar <56826955+AdamBeijar@users.noreply.github.com>
2020-10-05 14:52:47 +0300

    commit

--- shoutbox.php
+++ shoutbox.php
@@ -4,25 +4,30 @@
         $sql = "INSERT INTO shoutbox (username, comment) 
                 VALUES(:username, :comment)";
         $result = $conn->prepare($sql);
-        $res = $result->execute(
-            array(
-                ':username' => $_POST['user'],
-                ':comment' => $_POST['comment']
-            )
-        );
+        if (!empty($_POST['user'])  && !empty($_POST['comment'])) {
+            $res = $result->execute(
+                array(
+                    ':username' => $_POST['user'],
+                    ':comment' => $_POST['comment']
+                )
+            );
+        } else {
+            echo "skriv in ett namn din dumma jävel";
+        };
         $q_select = "SELECT * FROM shoutbox ORDER BY time DESC";
         $stmt = $conn->query($q_select);
-    }
+    };
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Shoutbox</title>
 </head>
 <body>
-    <form action="shoutbox.php" method="post">
+    <form method="post">
         <p>
             <label for="user">First name:</label><br>
             <input type="text" id="user" name="user" placeholder="Skriv ditt namn">
@@ -36,9 +41,9 @@
 <?php 
     echo "<ul>";
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "<li>" . "Username: ".$row['username'] . '<br>'
-        . "Time: " . $row['time'] . '<br>'
-        . "Comment: " . $row['comment'] . '<br><br><br></li>';
+        echo "<li>" . "<b>Username:</b> ".$row['username'] . '<br>'
+        . "<b>Time:</b> " . $row['time']. '<br>'
+        . "<b>Comment:</b> " . "<p class='comment'>" . nl2br($row['comment']) . '<br><br><br></p></li>';
     };
     echo "</ul>";
 ?>
